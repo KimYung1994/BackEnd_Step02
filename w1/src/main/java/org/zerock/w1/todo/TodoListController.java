9a12f8bdcbc872e59b1e1617328de81d2d098e0f
KimYung1994 <157556953+KimYung1994@users.noreply.github.com>
2024-04-09 09:50:39 +0900

    4월8일까지 수업내용 (2)

--- w1/src/main/java/org/zerock/w1/todo/TodoListController.java
+++ w1/src/main/java/org/zerock/w1/todo/TodoListController.java
@@ -13,6 +13,8 @@ public class TodoListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("/todo/list");
+        
+        System.out.println("DB로부터 목록을 꺼내어 list.jsp에 전달");
 
         req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
 
--- w1/src/main/java/org/zerock/w1/todo/service/TodoService.java
+++ w1/src/main/java/org/zerock/w1/todo/service/TodoService.java
@@ -1,2 +1,31 @@
-package org.zerock.w1.todo.service;public enum TodoService {
+package org.zerock.w1.todo.service;
+
+import org.zerock.w1.todo.dto.TodoDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public enum TodoService {
+    INSTANCE;
+
+    public void register(TodoDTO todoDTO) {
+
+        System.out.println("DEBUG...."+todoDTO);
+
+    }
+
+    public  List<TodoDTO> getList(){
+
+        List<TodoDTO> todoDTOS = IntStream.range(0, 10).mapToObj(i -> {
+            TodoDTO dto = new TodoDTO();
+            dto.setTno((long)i);
+            dto.setTitle("TOdo.." + i);
+            dto.setDueDate(LocalDate.now());
+
+            return dto;
+        }).collect(Collectors.toList());
+        return todoDTOS;
+    }
 }
